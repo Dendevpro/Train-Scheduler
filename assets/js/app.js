c5ceaabf43aaff5e0e0a2b0f07e62013e06f5f0c
Dendevpro <mauro.facciolla@gmail.com>
2019-01-20 01:48:12 -0800

    Created Framework, basic styling and Initialize Firebase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB6wOIcpCHFuEFzOUkFDaF7nKrygxwbYcg",
+    authDomain: "train-scheduler-f07ca.firebaseapp.com",
+    databaseURL: "https://train-scheduler-f07ca.firebaseio.com",
+    projectId: "train-scheduler-f07ca",
+    storageBucket: "train-scheduler-f07ca.appspot.com",
+    messagingSenderId: "182967098733"
+};
+firebase.initializeApp(config);
